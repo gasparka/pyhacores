01ea558319c5b00c549c47aa8d5c673fe15b8c32
petspats <gkarm@live.com>
2017-03-05 23:59:21 +0200

    add idol
    Former-commit-id: 65a2e152e0b0988f27a0a3be872f554055306b8a

---
+++ pyhacores/filter/fir/idol.py
@@ -0,0 +1,35 @@
+from scipy import signal
+
+from pyhacores.filter.fir.model import normalize_taps
+
+
+class FIR(HW):
+    """ FIR filter, taps will be normalized to sum 1 """
+    def __init__(self, taps, type):
+        self.taps = normalize_taps(taps)
+
+        # registers
+        self.mul = [Sfix()] * len(self.taps)
+        self.acc = [Sfix()] * len(self.taps)
+        self.out = Sfix()
+
+        # constants
+        self.taps_fix_reversed = Const([Sfix(x, 0, -17) for x in reversed(self.taps)])
+        self._delay = 3
+
+    def main(self, x):
+        """
+        Transposed form FIR implementation, uses full precision
+        """
+        for i in range(len(self.taps)):
+            self.next.mul[i] = x * self.taps[i]
+            if i == 0:
+                self.next.acc[0] = self.mul[i]
+            else:
+                self.next.acc[i] = self.acc[i - 1] + self.mul[i]
+
+        self.next.out = self.acc[-1]
+        return self.out
+
+    def model_main(self, x):
+        return signal.lfilter(self.taps, [1.0], x)
