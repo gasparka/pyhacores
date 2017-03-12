69fee4e45cd7a47227603b3a7388316af14985bd
petspats <gkarm@live.com>
2017-03-12 12:42:05 +0200

    overhoul
    Former-commit-id: acff641d1419a5e19aa9d34835675807bac2b4e2

--- pyhacores/util_complex/model.py
+++ pyhacores/util_complex/model.py
@@ -6,13 +6,13 @@ from pyha.common.sfix import resize, ComplexSfix, Sfix, fixed_truncate
 
 class Conjugate(HW):
     def __init__(self):
-        self.outreg = ComplexSfix()
+        self.outreg = ComplexSfix(0, 0, -17, round_style=fixed_truncate)
 
         self._delay = 1
 
     def main(self, x):
         self.next.outreg.real = x.real
-        self.next.outreg.imag = resize(-x.imag, size_res=x.imag)
+        self.next.outreg.imag = -x.imag
         return self.outreg
 
     def model_main(self, x):
@@ -23,37 +23,24 @@ class ComplexMultiply(HW):
     """ (x + yj)(u + vj) = (xu - yv) + (xv + yu)j """
 
     def __init__(self):
-        self.real_xu = Sfix()
-        self.real_yv = Sfix()
+        self.real_xu = Sfix(0, 0, -17, round_style=fixed_truncate)
+        self.real_yv = Sfix(0, 0, -17, round_style=fixed_truncate)
 
-        self.imag_xv = Sfix()
-        self.imag_yu = Sfix()
+        self.imag_xv = Sfix(0, 0, -17, round_style=fixed_truncate)
+        self.imag_yu = Sfix(0, 0, -17, round_style=fixed_truncate)
 
-        self.outreg = ComplexSfix()
+        self.outreg = ComplexSfix(0+0j, 0, -17, round_style=fixed_truncate)
 
         self._delay = 2
 
     def main(self, a, b):
-        # assert a.has_same_bounds(b)
-        # self.next.real_xu = a.real * b.real
-        # self.next.real_yv = a.imag * b.imag
-        # self.next.outreg.real = resize(self.real_xu - self.real_yv,
-        #                                size_res=a.real, round_style=fixed_truncate)
-        #
-        # self.next.imag_xv = a.real * b.imag
-        # self.next.imag_yu = a.imag * b.real
-        # self.next.outreg.imag = resize(self.imag_xv + self.imag_yu,
-        #                                size_res=a.imag, round_style=fixed_truncate)
+        self.next.real_xu = a.real * b.real
+        self.next.real_yv = a.imag * b.imag
+        self.next.outreg.real = self.real_xu - self.real_yv
 
-        self.next.real_xu = resize(a.real * b.real, size_res=a.real, round_style=fixed_truncate)
-        self.next.real_yv = resize(a.imag * b.imag, size_res=a.real, round_style=fixed_truncate)
-        self.next.outreg.real = resize(self.real_xu - self.real_yv,
-                                       size_res=a.real, round_style=fixed_truncate)
-
-        self.next.imag_xv = resize(a.real * b.imag, size_res=a.real, round_style=fixed_truncate)
-        self.next.imag_yu = resize(a.imag * b.real, size_res=a.real, round_style=fixed_truncate)
-        self.next.outreg.imag = resize(self.imag_xv + self.imag_yu,
-                                       size_res=a.real, round_style=fixed_truncate)
+        self.next.imag_xv = a.real * b.imag
+        self.next.imag_yu = a.imag * b.real
+        self.next.outreg.imag = self.imag_xv + self.imag_yu
 
         return self.outreg
 
