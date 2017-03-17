6fe75479e1492622cc0b32b9787a27e4d953d9ae
petspats <gkarm@live.com>
2017-03-17 18:42:23 +0200

    refactor cordic code and tests

--- pyhacores/cordic/test_.py
+++ pyhacores/cordic/test_.py
@@ -1,12 +1,8 @@
-from pathlib import Path
-
 import numpy as np
 import pytest
 from scipy.signal import chirp, hilbert
 
-from pyha.common.sfix import ComplexSfix, Sfix
-from pyha.simulation.simulation_interface import assert_sim_match, SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE, \
-    debug_assert_sim_match
+from pyha.simulation.simulation_interface import assert_sim_match, SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE
 
 from pyhacores.cordic.model import Cordic, ToPolar, Angle, Abs, NCO
 from pyhacores.cordic.model import CordicMode
@@ -91,68 +87,13 @@ class TestToPolar:
         assert_sim_match(dut, expect, inputs, atol=1e-4)
 
 
-    def test_angle_phantom_cmultconj(self):
-        # phantom 2 -> demod = c[1:] * np.conjugate(c[:-1])
-        path = Path(__file__).parent / 'data/phantom_cmult_conjugate.npy'
-        sig = np.load(str(path))
-
-        inputs = sig
-        expect = []
-
-        dut = Angle()
-
-        # assert_sim_match(dut, [ComplexSfix(left=0, right=-17)],
-        out = debug_assert_sim_match(dut, [ComplexSfix(left=0, right=-17)],
-                                     expect, inputs,
-                                     rtol=1e-4,
-                                     atol=1e-4,  # zeroes make trouble
-                                     )
-        np.testing.assert_allclose(out[0][200:], out[1][200:], 1e-3, 1e-4)
-        np.testing.assert_allclose(out[0][200:], out[1][200:], 1e-3, 1e-4)
-        # np.testing.assert_allclose(out[0], out[2], 1e-3, 1e-4)
-        # import matplotlib.pyplot as plt
-        # plt.plot(out[0], label='MODEL')
-        # plt.plot(out[1], label='HW_MODEL')
-        # plt.plot(out[2], label='RTL')
-        # plt.legend()
-        # plt.show()
-
-    def test_angle_phantom_cmultconj_mismatch(self):
-        # todo: first 100 samples, RTL and HW_SIM not matching!
-        pytest.xfail('Lazyness')
-
-        # phantom 2 -> demod = c[1:] * np.conjugate(c[:-1])
-        path = Path(__file__).parent / 'phantom_cmult_conjugate.npy'
-        sig = np.load(str(path))
-
-        inputs = sig
-        expect = []
-
-        dut = Angle()
-
-        # assert_sim_match(dut, [ComplexSfix(left=0, right=-17)],
-        out = debug_assert_sim_match(dut, [ComplexSfix(left=0, right=-17)],
-                                     expect, inputs,
-                                     rtol=1e-4,
-                                     atol=1e-4,  # zeroes make trouble
-                                     )
-        # np.testing.assert_allclose(out[0], out[1], 1e-3, 1e-4)
-        # np.testing.assert_allclose(out[0], out[2], 1e-3, 1e-4)
-        import matplotlib.pyplot as plt
-        plt.plot(out[0], label='MODEL')
-        plt.plot(out[1], label='HW_MODEL')
-        plt.plot(out[2], label='RTL')
-        plt.legend()
-        plt.show()
-
-
 class TestAngle:
     def test_basic(self):
         inputs = [np.exp(0.5j), np.exp(0.1j)]
         expect = [0.5 / np.pi, 0.1 / np.pi]
 
         dut = Angle()
-        assert_sim_match(dut, expect, inputs)
+        assert_sim_match(dut, expect, inputs, atol=1e-4)
 
     def test_chirp(self):
         analytic_signal, ref_abs, ref_instantaneous_phase = chirp_stimul()
@@ -182,28 +123,33 @@ class TestAbs:
         assert_sim_match(dut, expect, inputs)
 
 
-@pytest.mark.parametrize('period', [0.25, 0.50, 0.75, 1, 2, 4])
-def test_nco(period):
-    fs = 64
-    freq = 1
-    phase_inc = 2 * np.pi * freq / fs
-    phase_cumsum = np.arange(0, period * fs * phase_inc, phase_inc)
+class TestNCO:
+    def test_basic(self):
+        inputs = [0.01] * 4
+        expect = [np.exp(0.01j*np.pi), np.exp(0.02j*np.pi), np.exp(0.03j*np.pi), np.exp(0.04j*np.pi)]
 
-    ref = np.exp(phase_cumsum * 1j)
+        dut = NCO()
+        assert_sim_match(dut, expect, inputs)
 
-    pil = np.diff(phase_cumsum) / np.pi
-    pil = np.insert(pil, 0, [0.0])
+    @pytest.mark.parametrize('period', [0.25, 0.50, 0.75, 1, 2, 4])
+    def test_nco(self, period):
+        fs = 64
+        freq = 1
+        phase_inc = 2 * np.pi * freq / fs
+        phase_cumsum = np.arange(0, period * fs * phase_inc, phase_inc)
 
-    inputs = pil
-    expect = ref
+        ref = np.exp(phase_cumsum * 1j)
 
-    dut = NCO()
-    sims = [SIM_MODEL, SIM_HW_MODEL, SIM_RTL]
-    if period == 4:
-        sims = [SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE]
-    # outs = debug_assert_sim_match(dut, [Sfix(left=0, right=-24)],
-    assert_sim_match(dut, [Sfix(left=0, right=-18)],
-                     expect, inputs,
-                     rtol=1e-4,
-                     simulations=sims
-                     )
+        pil = np.diff(phase_cumsum) / np.pi
+        pil = np.insert(pil, 0, [0.0])
+
+        inputs = pil
+        expect = ref
+
+        dut = NCO()
+        sims = [SIM_MODEL, SIM_HW_MODEL, SIM_RTL]
+        if period == 4:
+            sims = [SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE]
+
+        assert_sim_match(dut, expect, inputs, simulations=sims)
+
