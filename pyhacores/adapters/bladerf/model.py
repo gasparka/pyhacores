125b21c76c993d6beb98f035ec368c9ebcbf35c7
petspats <gkarm@live.com>
2017-03-17 18:55:16 +0200

    refactor blade adapter
    Former-commit-id: bf1381fc801ab594a4838f961a93ff70f0e99294

--- pyhacores/adapters/bladerf/model.py
+++ pyhacores/adapters/bladerf/model.py
@@ -1,8 +1,8 @@
 from pyha.common.hwsim import HW
-from pyha.common.sfix import resize, scalb, ComplexSfix, Sfix
+from pyha.common.sfix import scalb, ComplexSfix, Sfix
 
 
-class ComplexSource(HW):
+class Source(HW):
     """ Convert BladeRF style I/Q into Pyha Complex type
     (4 downto -11) -> (0 downto -17)"""
 
@@ -11,25 +11,29 @@ class ComplexSource(HW):
 
         self._delay = 1
 
-    def main(self, i, q):
-        self.next.out.real = scalb(i, 4)
-        self.next.out.imag = scalb(q, 4)
+    def main(self, real, imag):
+        self.next.out.real = scalb(real, 4)
+        self.next.out.imag = scalb(imag, 4)
         return self.out
 
     def model_main(self, i, q):
         return i * (2 ** 4) + q * (2 ** 4) * 1j
 
 
-class FloatSink(HW):
-    """ Pyha 18 bit signal to BladeRF style """
+class Sink(HW):
+    """ Default ComplexSfix to BladeRF style """
     def __init__(self):
-        self.out = Sfix(0, 0, -15)
+        self.out_real = Sfix(0, 0, -15)
+        self.out_imag = Sfix(0, 0, -15)
 
         self._delay = 1
 
-    def main(self, x):
-        self.next.out = scalb(x, -4)
-        return self.out
+    def main(self, c):
+        self.next.out_real = scalb(c.real, -4)
+        self.next.out_imag = scalb(c.imag, -4)
+        return self.out_real, self.out_imag
 
-    def model_main(self, x):
-        return x * (2 ** -4)
+    def model_main(self, c):
+        real = [x.real * (2 ** -4) for x in c]
+        imag = [x.imag * (2 ** -4) for x in c]
+        return real, imag
