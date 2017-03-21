859898464606c83dcd309c9409f706365351905b
petspats <gkarm@live.com>
2017-03-21 14:59:06 +0200

    alternitive implementation
    Former-commit-id: 3d6bc01e196cd9d6c811a894455a5c5fc5429f37

--- pyhacores/under_construction/clock_recovery/gardner.py
+++ pyhacores/under_construction/clock_recovery/gardner.py
@@ -1,10 +1,15 @@
+from math import floor
+
+import pytest
+
 from pyhacores.under_construction.interpolator.model import Interpolator
 
 
 class GardnerTimingRecovery:
     def __init__(self, sps):
         # sps must be divisible by 2 -> required by gardner
-        self.mu = 1.0
+        assert sps in [2, 4]
+        self.d = sps - 1
         self.sps = sps
         self.interpolator = Interpolator()
         self.out_int = [0.0] * self.sps
@@ -12,34 +17,61 @@ class GardnerTimingRecovery:
 
     def model_main(self, xlist):
         err_debug = []
-        ret = []
         mu_debug = []
-        d = self.sps - 1
+        ret = []
 
         for sample in xlist:
-            i_sample = self.interpolator.filter(sample, self.mu)
+            i_sample = self.interpolator.filter(sample, self.d % 1)
             self.out_int.append(i_sample)
 
             self.sps_counter += 1
             if self.sps_counter >= self.sps:
                 self.sps_counter = 0
-                e = (self.out_int[-1 - d] - self.out_int[-self.sps - d]) * self.out_int[-self.sps // 2 - d]
-                self.mu -= e / 4
-                if self.mu < 0.0:
-                    tmp = self.mu
-                    self.mu = -self.mu
-                    # self.mu = 0.0
-                    d += 1
-                    print(f'<d:{d} mu_in:{tmp:.2f} mu{self.mu:.2f}')
-                if self.mu > 1.0:
-                    # self.mu = 0.0
-                    tmp = self.mu
-                    self.mu = self.mu - 1.0
-                    d += 1
-                    print(f'>d:{d} mu_in:{tmp:.2f} mu{self.mu:.2f}')
 
-                mu_debug.append(self.mu)
+                intd = int(floor(self.d))
+                e = (self.out_int[-1 - intd] - self.out_int[-self.sps - intd]) * self.out_int[-self.sps // 2 - intd]
+                self.d += e / 4
+
+                mu_debug.append(self.d)
                 err_debug.append(e)
-                ret.append(self.out_int[-1 - d + 1])
+                ret.append(self.out_int[-1 - intd + 1])
 
         return ret, err_debug, mu_debug
+
+        # def model_main(self, xlist):
+        #     err_debug = []
+        #     ret = []
+        #     mu_debug = []
+        #     d = self.sps - 1
+        #
+        #     for sample in xlist:
+        #         i_sample = self.interpolator.filter(sample, self.mu)
+        #         self.out_int.append(i_sample)
+        #
+        #         self.sps_counter += 1
+        #         if self.sps_counter >= self.sps:
+        #             self.sps_counter = 0
+        #             e = (self.out_int[-1 - d] - self.out_int[-self.sps - d]) * self.out_int[-self.sps // 2 - d]
+        #             self.mu += e / 4
+        #             if self.mu < 0.0:
+        #                 # pytest.xfail()
+        #                 tmp = self.mu
+        #                 # self.mu = self.mu + 1.0
+        #                 self.mu = 0.5
+        #                 d -= 1
+        #                 print(f'<d:{d} mu_in:{tmp:.2f} mu{self.mu:.2f}')
+        #             if self.mu > 1.0:
+        #                 # self.mu = 0.0
+        #                 tmp = self.mu
+        #                 # self.mu = self.mu - 1.0
+        #                 self.mu = 0.5
+        #                 d += 1
+        #                 print(f'>d:{d} mu_in:{tmp:.2f} mu{self.mu:.2f}')
+        #
+        #             mu_debug.append(self.mu)
+        #             err_debug.append(e)
+        #             ret.append(self.out_int[-1 - d + 1])
+        #
+        #
+        #
+        #     return ret, err_debug, mu_debug
