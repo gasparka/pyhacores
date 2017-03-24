e329d2a26350ea4222d1e06752e61b6b6709a9ba
petspats <gkarm@live.com>
2017-03-24 19:40:29 +0200

    hw_model and test

--- pyhacores/under_construction/clock_recovery/simple_gardner/test_.py
+++ pyhacores/under_construction/clock_recovery/simple_gardner/test_.py
@@ -1,4 +1,5 @@
 from astropy.tests.helper import pytest
+from pyha.common.sfix import Sfix
 from pyha.common.util import bools_to_bitstr, hex_to_bitstr
 from pyha.simulation.simulation_interface import debug_assert_sim_match, SIM_HW_MODEL, SIM_MODEL
 
@@ -6,6 +7,9 @@ from pyhacores.under_construction.clock_recovery.simple_gardner.gardner import S
 from pyhacores.under_construction.clock_recovery.test_ import data_gen
 import numpy as np
 import matplotlib.pyplot as plt
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 50, 10
 
 
 class TestSimpleGardnerTimingRecovery:
@@ -23,29 +27,44 @@ class TestSimpleGardnerTimingRecovery:
         self.sps = sps
         packet = '123456789abcdef'
         insig = data_gen(f'aaaaaa{packet}aa', self.sps, int_delay, fract_delay, noise_amp)
-        recover = SimpleGardnerTimingRecovery(self.sps)
+        dut = SimpleGardnerTimingRecovery(self.sps)
 
-        ret, err, mu = recover.model_main(insig)
-        # plt.plot(ret, label='ret')
-        # plt.plot(err, label='err')
-        # plt.plot(mu, label='mu')
-        # plt.grid()
-        # plt.legend()
+        r = debug_assert_sim_match(dut, None, insig, simulations=[SIM_MODEL, SIM_HW_MODEL],
+                                   types=[Sfix(0, 1, -32)])
+
+
+        rt = np.transpose([x for x in np.transpose(r[1]) if x[3]])
+
+        # plt.subplot(1, 3, 1)
+        # plt.plot(rt[0])
+        # plt.plot(r[0][0])
+        #
+        # plt.subplot(1, 3, 2)
+        # plt.plot(rt[1])
+        # plt.plot(r[0][1])
+        #
+        # plt.subplot(1, 3, 3)
+        # plt.plot(rt[2])
+        # plt.plot(r[0][2])
+        #
         # plt.show()
-
-        bits = bools_to_bitstr([1 if x > 0 else 0 for x in ret])
         packet_bits = hex_to_bitstr(packet)
-        assert packet_bits in bits
+
+        bits_model = bools_to_bitstr([1 if x > 0 else 0 for x in r[0][0]])
+        bits_hwmodel = bools_to_bitstr([1 if x > 0 else 0 for x in rt[0]])
+        assert packet_bits in bits_model
+        assert packet_bits in bits_hwmodel
 
 
 
 def test_hw_model():
     sps = 8
     packet = '123456789abcdef'
-    insig = data_gen(f'aaaaaa{packet}aa', sps, 1, 0.3, 0)
+    insig = data_gen(f'aaaaaa{packet}aa', sps, 1, 0.9, 0.3)
     dut = SimpleGardnerTimingRecovery(sps)
 
-    r = debug_assert_sim_match(dut, None, insig, simulations=[SIM_MODEL, SIM_HW_MODEL])
+    r = debug_assert_sim_match(dut, None, insig, simulations=[SIM_MODEL, SIM_HW_MODEL],
+                               types=[Sfix(0,1,-32)])
 
     rt = np.transpose(r[1])
 
@@ -53,8 +72,8 @@ def test_hw_model():
     from pylab import rcParams
     rcParams['figure.figsize'] = 50, 10
     plt.subplot(1,3,1)
-    plt.plot(r[0][0])
     plt.plot(rt[0])
+    plt.plot(r[0][0])
 
     plt.subplot(1, 3, 2)
     plt.plot(rt[1])
