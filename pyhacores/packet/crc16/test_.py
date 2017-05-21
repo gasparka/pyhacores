efe7ae23763e4577af73256e4aacee459cf2c04f
petspats <gkarm@live.com>
2017-05-21 23:15:11 +0300

    remove model code for crc
    Former-commit-id: 75c5a1944c1431ac08bbc1d872d86391815fb7ab

--- pyhacores/packet/crc16/test_.py
+++ pyhacores/packet/crc16/test_.py
@@ -11,16 +11,13 @@ class TestCRC16:
     def test_simple_one(self):
         data = hex_to_bool_list('8dfc4ff97dffdb11ff438aee29243910365e908970b9475e')
         reload = [False] * len(data)
-        model = self.dut.model_main(data, reload)
-        assert model[-1] == 0
+
         assert_sim_match(self.dut, None, data, reload)
 
     def test_reset(self):
         data = hex_to_bool_list('A8dfc4ff97dffdb11ff438aee2524391039a4908970b91cdb')
         reload = [False, False, False, False, True] + [False] * 191
 
-        model = self.dut.model_main(data, reload)
-        assert model[-1] == 0
 
         assert_sim_match(self.dut, None, data, reload)
 
@@ -28,9 +25,5 @@ class TestCRC16:
         data = hex_to_bool_list('8dfc4ff97dffdb11ff438aee2524391039a4908970b91cdb'
                                 '8dfc4ff97dffdb11ff438aee2524391039a4908970b91cdb')
         reload = [False] * 192 + [True] + [False] * 191
-
-        model = self.dut.model_main(data, reload)
-        assert model[191] == 0
-        assert model[-1] == 0
 
         assert_sim_match(self.dut, None, data, reload)
