76f78b7a58c9d8b91f91f587883022060bd071ff
gaspar <gkarm@live.com>
2017-11-06 19:32:35 +0200

    prep for semi-auto fix conversion
    Former-commit-id: f7c0f71d1f85039a0696b09f7bfa3ba7f8993092

--- pyhacores/filter/fir/model.py
+++ pyhacores/filter/fir/model.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 class FIR(Hardware):
-    """ FIR filter, taps will be normalized to sum 1 """
+    """ Transposed FIR filter """
     def __init__(self, taps):
-        self.taps = np.array(taps).tolist()
+        self.taps = taps
 
         # registers
         self.acc = [Sfix(left=0, round_style=fixed_truncate, overflow_style=fixed_wrap)] * (len(self.taps) + 1)
@@ -16,6 +16,13 @@ class FIR(Hardware):
         # constants
         self.TAPS_REVERSED = [Sfix(x, 0, -17) for x in reversed(self.taps)]
         self.DELAY = 1
+
+        # # constants
+        # self.DELAY = 1
+        # self.TAPS = taps
+        #
+        # # registers
+        # self.acc = [0.0] * (len(self.TAPS) + 1)
 
     def main(self, x):
         for i in range(1, len(self.acc)):
