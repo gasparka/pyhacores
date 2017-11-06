6e69dedd698892ad3bc141f10e16889402bf710e
gaspar <gkarm@live.com>
2017-11-06 18:35:56 +0200

    simplify the FIR code

--- pyhacores/filter/fir/model.py
+++ pyhacores/filter/fir/model.py
@@ -1,48 +1,27 @@
-from pyha.common.hwsim import HW
+from pyha.common.hwsim import Hardware
 from pyha.common.sfix import Sfix, fixed_truncate, fixed_wrap
 
 from scipy import signal
 import numpy as np
 
 
-def rescale_taps(taps):
-    """
-    Rescale taps in that way that their sum equals 1
-    """
-    taps = np.array(taps)
-    cs = sum(taps)
-    # fixme: not sure here, abs seems right as it avoids overflows in core,
-    # then again it reduces the fir gain
-    # cs = sum(abs(taps))
-    for (i, x) in enumerate(taps):
-        taps[i] = x / cs
-
-    return taps.tolist()
-
-
-class FIR(HW):
+class FIR(Hardware):
     """ FIR filter, taps will be normalized to sum 1 """
     def __init__(self, taps):
-        self.taps = rescale_taps(taps)
+        self.taps = np.array(taps).tolist()
 
         # registers
-        self.acc = [Sfix(left=1, round_style=fixed_truncate, overflow_style=fixed_wrap)] * len(self.taps)
-        self.out = Sfix(0, 0, -17, round_style=fixed_truncate)
+        self.acc = [Sfix(left=0, round_style=fixed_truncate, overflow_style=fixed_wrap)] * (len(self.taps) + 1)
 
         # constants
         self.TAPS_REVERSED = [Sfix(x, 0, -17) for x in reversed(self.taps)]
-        self.DELAY = 2
+        self.DELAY = 1
 
     def main(self, x):
-        """
-        Transposed form FIR implementation, this implementation has problems if you plan to rapidly switch the taps.
-        """
-        self.acc[0] = x * self.TAPS_REVERSED[0]
-        for i in range(1, len(self.TAPS_REVERSED)):
-            self.acc[i] = self.acc[i - 1] + x * self.TAPS_REVERSED[i]
+        for i in range(1, len(self.acc)):
+            self.acc[i] = self.acc[i - 1] + x * self.TAPS_REVERSED[i - 1]
 
-        self.out = self.acc[-1]
-        return self.out
+        return self.acc[-1]
 
     def model_main(self, x):
         return signal.lfilter(self.taps, [1.0], x)
