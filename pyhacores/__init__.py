d1d1f0b178247674ea553cb1a8fbdf4c8a0df657
gaspar <gkarm@live.com>
2017-12-04 19:52:19 +0200

    refactor cordic
    Former-commit-id: ec1defc50a2000e9cb14e959a05af11bd738d2e9

---
+++ pyhacores/__init__.py
@@ -0,0 +1,5 @@
+from .adapters.blade_rf import BladeRFSink, BladeRFSource
+
+from .cordic.cordic_core import Cordic, CordicMode
+from .cordic.to_polar import ToPolar, Angle, Abs
+from .cordic.nco import NCO
--- scripts/install_cocotb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -vx
-
-# TODO: create PR and use original cocotb repo!
-git clone https://github.com/petspats/cocotb
