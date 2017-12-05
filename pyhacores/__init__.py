3128affef9d7a9213b588909735fbdcdd326fc74
gaspar <gkarm@live.com>
2017-12-05 14:27:49 +0200

    move moving average to filters
    Former-commit-id: d500d9b46f880220db904b0bb3753b838b66b8bb

--- pyhacores/__init__.py
+++ pyhacores/__init__.py
@@ -3,3 +3,5 @@ from .adapters.blade_rf import BladeRFSink, BladeRFSource
 from .cordic.cordic_core import Cordic, CordicMode
 from .cordic.to_polar import ToPolar, Angle, Abs
 from .cordic.nco import NCO
+
+from .filter.moving_average import MovingAverage
