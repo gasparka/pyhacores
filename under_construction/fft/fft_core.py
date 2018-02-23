474bedaaf3ef5ef1234d6480ed725404744fe71e
gaspar <gkarm@live.com>
2018-02-23 20:12:45 +0200

    GATE 50Mhz

--- under_construction/fft/fft_core.py
+++ under_construction/fft/fft_core.py
@@ -12,43 +12,47 @@ def W(k, N):
 
 class StageR2SDF(Hardware):
     def __init__(self, fft_size):
-        self.fft_size = fft_size
-        self.fft_half = fft_size // 2
+        self.FFT_SIZE = fft_size
+        self.FFT_HALF = fft_size // 2
 
-        self.control_mask = (self.fft_half - 1)
-        self.shr = [Complex() for _ in range(self.fft_half)]
+        self.CONTROL_MASK = (self.FFT_HALF - 1)
+        self.shr = [Complex() for _ in range(self.FFT_HALF)]
 
-        self.TWIDDLES = [W(i, self.fft_size) for i in range(self.fft_half)]
+
+        self.TWIDDLES = [W(i, self.FFT_SIZE) for i in range(self.FFT_HALF)]
 
     def main(self, x, control):
-        if not (control & self.fft_half):
+
+        up_real = resize(self.shr[-1].real + x.real, 0, -17)
+        up_imag = resize(self.shr[-1].imag + x.imag, 0, -17)
+        up = Complex(up_real, up_imag)
+        # up = self.shr[-1] + x
+
+        # down sub
+        down_sub_real = resize(self.shr[-1].real - x.real, 0, -17)
+        down_sub_imag = resize(self.shr[-1].imag - x.imag, 0, -17)
+
+        twiddle = self.TWIDDLES[control & self.CONTROL_MASK]
+        down_real = resize(resize((down_sub_real * twiddle.real), 0, -17) - resize((down_sub_imag * twiddle.imag), 0, -17), 0, -17)
+        down_imag = resize(resize((down_sub_real * twiddle.imag), 0, -17) + resize((down_sub_imag * twiddle.real), 0, -17), 0, -17)
+        down = Complex(down_real, down_imag)
+        # down = (self.shr[-1] - x) * self.TWIDDLES[control & self.control_mask]
+
+        if not (control & self.FFT_HALF):
             self.shr = [x] + self.shr[:-1]
             return self.shr[-1]
         else:
-            up_real = resize(self.shr[-1].real + x.real, 0, -17)
-            up_imag = resize(self.shr[-1].imag + x.imag, 0, -17)
-            up = Complex(up_real, up_imag)
-            # up = self.shr[-1] + x
-
-            # down sub
-            down_sub_real = resize(self.shr[-1].real - x.real, 0, -17)
-            down_sub_imag = resize(self.shr[-1].imag - x.imag, 0, -17)
-
-            twiddle = self.TWIDDLES[control & self.control_mask]
-            down_real = resize((down_sub_real * twiddle.real) - (down_sub_imag * twiddle.imag), 0, -17)
-            down_imag = resize((down_sub_real * twiddle.imag) + (down_sub_imag * twiddle.real), 0, -17)
-            down = Complex(down_real, down_imag)
-            # down = (self.shr[-1] - x) * self.TWIDDLES[control & self.control_mask]
             self.shr = [down] + self.shr[:-1]
             return up
 
 
 class R2SDF(Hardware):
     def __init__(self, fft_size):
-        self.fft_size = fft_size
+        self.FFT_SIZE = fft_size
 
-        self.n_bits = int(np.log2(fft_size))
+        # self.n_bits = int(np.log2(fft_size))
         # self.stages = [StageR2SDF(2 ** (pow + 1)) for pow in reversed(range(self.n_bits))]
+        self.stage32 = StageR2SDF(32)
         self.stage16 = StageR2SDF(16)
         self.stage8 = StageR2SDF(8)
         self.stage4 = StageR2SDF(4)
@@ -59,10 +63,13 @@ class R2SDF(Hardware):
         self.DELAY = fft_size - 1
 
     def main(self, x):
-        next_control = (self.control + 1) % self.fft_size
+        next_control = self.control + 1
+        if next_control == self.FFT_SIZE:
+            next_control = 0
 
         # execute stages
         out = x
+        out = self.stage32.main(out, self.control)
         out = self.stage16.main(out, self.control)
         out = self.stage8.main(out, self.control)
         out = self.stage4.main(out, self.control)
@@ -76,7 +83,7 @@ class R2SDF(Hardware):
 
     def model_main(self, x):
         from scipy.fftpack import fft
-        x = np.array(x).reshape((-1, self.fft_size))
+        x = np.array(x).reshape((-1, self.FFT_SIZE))
         stack = np.hstack(fft(x))
 
         # apply bit reversing ie. mess up the output order to match radix-2 algorithm
@@ -88,20 +95,20 @@ class R2SDF(Hardware):
         def bit_reversed_indexes(N):
             return [bit_reverse(i, int(np.log2(N))) for i in range(N)]
 
-        rev_index = bit_reversed_indexes(self.fft_size)
+        rev_index = bit_reversed_indexes(self.FFT_SIZE)
         if len(stack.shape) == 1:
             return stack[rev_index]
         assert 0
 
 
 def test_conv():
-    fft_size = 16
+    fft_size = 32
     dut = R2SDF(fft_size)
     inp = np.random.uniform(-1, 1, fft_size) + np.random.uniform(-1, 1, fft_size) * 1j
     inp *= 0.25 / 2
 
     sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
-                                           'RTL'
+                                           'RTL', 'GATE'
                                            ], conversion_path='/home/gaspar/git/pyhacores/playground')
     assert sims_close(sims, rtol=1e-2)
 
