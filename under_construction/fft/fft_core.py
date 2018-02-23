7731b19cbe815254fe736d5843ea2250d443c376
gaspar <gkarm@live.com>
2018-02-23 18:56:36 +0200

    first RTL version

--- under_construction/fft/fft_core.py
+++ under_construction/fft/fft_core.py
@@ -1,7 +1,7 @@
 import timeit
 
 import pytest
-from pyha import Hardware, simulate, sims_close, Complex
+from pyha import Hardware, simulate, sims_close, Complex, resize
 import numpy as np
 
 
@@ -18,13 +18,27 @@ class StageR2SDF(Hardware):
         self.control_mask = (self.fft_half - 1)
         self.shr = [Complex() for _ in range(self.fft_half)]
 
+        self.TWIDDLES = [W(i, self.fft_size) for i in range(self.fft_half)]
+
     def main(self, x, control):
         if not (control & self.fft_half):
             self.shr = [x] + self.shr[:-1]
             return self.shr[-1]
         else:
-            up = self.shr[-1] + x
-            down = (self.shr[-1] - x) * W(control & self.control_mask, self.fft_size)
+            up_real = resize(self.shr[-1].real + x.real, 0, -17)
+            up_imag = resize(self.shr[-1].imag + x.imag, 0, -17)
+            up = Complex(up_real, up_imag)
+            # up = self.shr[-1] + x
+
+            # down sub
+            down_sub_real = resize(self.shr[-1].real - x.real, 0, -17)
+            down_sub_imag = resize(self.shr[-1].imag - x.imag, 0, -17)
+
+            twiddle = self.TWIDDLES[control & self.control_mask]
+            down_real = resize((down_sub_real * twiddle.real) - (down_sub_imag * twiddle.imag), 0, -17)
+            down_imag = resize((down_sub_real * twiddle.imag) + (down_sub_imag * twiddle.real), 0, -17)
+            down = Complex(down_real, down_imag)
+            # down = (self.shr[-1] - x) * self.TWIDDLES[control & self.control_mask]
             self.shr = [down] + self.shr[:-1]
             return up
 
@@ -34,7 +48,12 @@ class R2SDF(Hardware):
         self.fft_size = fft_size
 
         self.n_bits = int(np.log2(fft_size))
-        self.stages = [StageR2SDF(2 ** (pow + 1)) for pow in reversed(range(self.n_bits))]
+        # self.stages = [StageR2SDF(2 ** (pow + 1)) for pow in reversed(range(self.n_bits))]
+        self.stage16 = StageR2SDF(16)
+        self.stage8 = StageR2SDF(8)
+        self.stage4 = StageR2SDF(4)
+        self.stage2 = StageR2SDF(2)
+
         self.control = 0
 
         self.DELAY = fft_size - 1
@@ -44,8 +63,13 @@ class R2SDF(Hardware):
 
         # execute stages
         out = x
-        for stage in self.stages:
-            out = stage.main(out, self.control)
+        out = self.stage16.main(out, self.control)
+        out = self.stage8.main(out, self.control)
+        out = self.stage4.main(out, self.control)
+        out = self.stage2.main(out, self.control)
+        # out = x
+        # for stage in self.stages:
+        #     out = stage.main(out, self.control)
 
         self.control = next_control
         return out
@@ -70,8 +94,20 @@ class R2SDF(Hardware):
         assert 0
 
 
+def test_conv():
+    fft_size = 16
+    dut = R2SDF(fft_size)
+    inp = np.random.uniform(-1, 1, fft_size) + np.random.uniform(-1, 1, fft_size) * 1j
+    inp *= 0.25 / 2
+
+    sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
+                                           'RTL'
+                                           ], conversion_path='/home/gaspar/git/pyhacores/playground')
+    assert sims_close(sims, rtol=1e-2)
+
+
 @pytest.mark.parametrize("fft_size", [2, 4, 8, 16, 32, 64, 128, 256, 512])
-def test_fft32(fft_size):
+def test_fft(fft_size):
     dut = R2SDF(fft_size)
     inp = np.random.uniform(-1, 1, fft_size) + np.random.uniform(-1, 1, fft_size) * 1j
 
