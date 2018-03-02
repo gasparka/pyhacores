3fc73a9fe690a9a20eefee9932609f92f77b9e80
gaspar <gkarm@live.com>
2018-03-02 15:25:59 +0200

    conjmult: synthesisable implementation

--- under_construction/fft/conjmult.py
+++ under_construction/fft/conjmult.py
@@ -1,19 +1,26 @@
 import numpy as np
 import pytest
-from pyha import Hardware, simulate, sims_close, Complex, resize, Sfix
+from pyha import Hardware, simulate, sims_close, Complex, resize, Sfix, right_index, left_index
 
 
 def conjugate(x):
     imag = resize(-x.imag, x.imag.left, x.imag.right)
     return Complex(x.real, imag)
 
+
 class ConjMult(Hardware):
     def __init__(self):
         self.out = Sfix(0, 0, -17)
         self.DELAY = 1
 
+    def conjugate(self, x):
+        imag = resize(-x.imag, left_index(x.imag), right_index(x.imag))
+        return Complex(x.real, imag)
+
     def main(self, complex_in):
-        self.out = (conjugate(complex_in) * complex_in).real
+        conj = self.conjugate(complex_in)
+        self.out = (conj.real * complex_in.real) - (conj.imag * complex_in.imag)
+        # self.out = (self.conjugate(complex_in) * complex_in).real
         return self.out
 
 
@@ -23,5 +30,5 @@ def test_abs():
     inp = np.random.uniform(-1, 1, M) + np.random.uniform(-1, 1, M) * 1j
     inp *= 0.5
 
-    sims = simulate(dut, inp, simulations=['MODEL_PYHA', 'PYHA'])
+    sims = simulate(dut, inp, simulations=['MODEL_PYHA', 'PYHA', 'RTL'])
     assert sims_close(sims)
