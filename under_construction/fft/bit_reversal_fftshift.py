48787300b90b48259ea9bfcfb9a7ac089cb65027
gaspar <gkarm@live.com>
2018-03-06 11:57:06 +0200

    bit_reversal: upgrade to new dataflow

--- under_construction/fft/bit_reversal_fftshift.py
+++ under_construction/fft/bit_reversal_fftshift.py
@@ -2,8 +2,7 @@ import pytest
 from pyha import Hardware, simulate, sims_close, Sfix, Complex
 import numpy as np
 from copy import copy
-
-from pyha.common.stream import Stream
+from under_construction.fft.packager import DataWithIndex
 
 
 def bit_reverse(x, n_bits):
@@ -19,13 +18,13 @@ class BitReversal(Hardware):
     def __init__(self, fft_size, fftshift=True):
         self.FFTSHIFT = fftshift
         self.FFT_SIZE = fft_size
-        self.control = 0
+        self.N_BITS = int(np.log2(fft_size))
+        self.DELAY = fft_size + 1
+
         self.mem0 = [Complex() for _ in range(fft_size)]
         self.mem1 = [Complex() for _ in range(fft_size)]
         self.state = False
-        self.N_BITS = int(np.log2(fft_size))
-
-        self.DELAY = fft_size
+        self.out = DataWithIndex(Complex(0.0, 0, -17), 0)
 
     def bit_reverse(self, control):
         cfix = Sfix(control, self.N_BITS, 0)
@@ -35,16 +34,12 @@ class BitReversal(Hardware):
 
         return int(ret)
 
-    def main(self, stream_in):
-        read_index = self.control
-        write_index = self.control
-        self.control += 1
-        if stream_in.package_end:
-            self.control = 0
-            self.state = not self.state
+    def main(self, inp):
+        read_index = inp.index
+        write_index = inp.index
 
         if self.FFTSHIFT:
-            if self.control < self.FFT_SIZE // 2:
+            if inp.index < self.FFT_SIZE // 2:
                 read_index += self.FFT_SIZE // 2
             else:
                 read_index -= self.FFT_SIZE // 2
@@ -52,13 +47,18 @@ class BitReversal(Hardware):
         read_index = self.bit_reverse(read_index)
 
         if self.state:
-            self.mem0[write_index] = stream_in.data
+            self.mem0[write_index] = inp.data
             ret = self.mem1[read_index]
         else:
-            self.mem1[write_index] = stream_in.data
+            self.mem1[write_index] = inp.data
             ret = self.mem0[read_index]
 
-        return Stream(ret, valid=True, package_start=self.control == 0, package_end=self.control == self.FFT_SIZE - 1)
+        if inp.index == self.FFT_SIZE - 1:
+            self.state = not self.state
+
+        self.out.data = ret
+        self.out.index = inp.index
+        return self.out
 
     def model_main(self, complex_in_list):
         ret = []
@@ -74,14 +74,17 @@ class BitReversal(Hardware):
 @pytest.mark.parametrize("fftshift", [True, False])
 @pytest.mark.parametrize("N", [2, 4, 8, 16, 32, 64, 128, 256])
 def test_bit_reversal(N, fftshift):
-    packages = np.random.randint(1, 4)
+    packages = np.random.randint(2, 4)
     inp = np.random.uniform(-1, 1, (packages, N)) + np.random.uniform(-1, 1, (packages, N)) * 1j
-    inp[0] = inp[0][bit_reversed_indexes(N)]
+    inp = [x[bit_reversed_indexes(N)] for x in inp]
+
     dut = BitReversal(N, fftshift)
     sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
-                                           'RTL',
+                                           # 'RTL',
                                            # 'GATE'
-                                           ])
+                                           ],
+                    output_callback=DataWithIndex.unpack,
+                    input_callback=DataWithIndex.pack)
     assert sims_close(sims)
 
 
@@ -90,10 +93,12 @@ def test_simple():
     inp = np.array(list(range(N))).astype(complex) / 100
     inp = inp[bit_reversed_indexes(N)]
     inp = [inp, inp]
-    # inp = np.expand_dims(inp, axis=0)
     dut = BitReversal(N)
     sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
                                            # 'RTL',
                                            # 'GATE'
-                                           ])
+                                           ],
+                    output_callback=DataWithIndex.unpack,
+                    input_callback=DataWithIndex.pack
+                    )
     assert sims_close(sims)
