15543250ac713dd40cf84b54df46a324f8e3e145
gaspar <gkarm@live.com>
2018-03-06 12:06:08 +0200

    conjmult: upgrade to new dataflow

--- under_construction/fft/conjmult.py
+++ under_construction/fft/conjmult.py
@@ -1,28 +1,36 @@
 import numpy as np
 import pytest
 from pyha import Hardware, simulate, sims_close, Complex, resize, Sfix, right_index, left_index
+from under_construction.fft.packager import DataWithIndex
+
 
 class ConjMult(Hardware):
     def __init__(self):
-        self.out = Sfix(0, 0, -17)
+        self.out = DataWithIndex(Sfix(0.0, 0, -17), 0)
         self.DELAY = 1
 
     def conjugate(self, x):
         imag = resize(-x.imag, left_index(x.imag), right_index(x.imag))
         return Complex(x.real, imag)
 
-    def main(self, complex_in):
-        conj = self.conjugate(complex_in)
-        self.out = (conj.real * complex_in.real) - (conj.imag * complex_in.imag)
+    def main(self, inp):
+        conj = self.conjugate(inp.data)
+        self.out.data = (conj.real * inp.data.real) - (conj.imag * inp.data.imag)
+        self.out.index = inp.index
         # self.out = (self.conjugate(complex_in) * complex_in).real
         return self.out
+
+    def model_main(self, data):
+        return (np.conjugate(data) * data).real
 
 
 def test_abs():
     M = 128
     dut = ConjMult()
-    inp = np.random.uniform(-1, 1, M) + np.random.uniform(-1, 1, M) * 1j
+    inp = np.random.uniform(-1, 1, size=(2, M)) + np.random.uniform(-1, 1, size=(2, M)) * 1j
     inp *= 0.5
 
-    sims = simulate(dut, inp, simulations=['MODEL_PYHA', 'PYHA', 'RTL'])
+    sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
+                                           # 'RTL'
+                                           ], output_callback=DataWithIndex.unpack, input_callback=DataWithIndex.pack)
     assert sims_close(sims)
