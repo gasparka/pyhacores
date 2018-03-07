e0ba7a49ed2f162a3f6291c951ddc2ebfdf32cf9
gaspar <gkarm@live.com>
2018-03-07 19:26:45 +0200

    test reduced bitwidths

--- under_construction/fft/fft_core.py
+++ under_construction/fft/fft_core.py
@@ -24,12 +24,9 @@ class StageR2SDF(Hardware):
         self.CONTROL_MASK = (self.FFT_HALF - 1)
         self.shr = [Complex() for _ in range(self.FFT_HALF)]
 
-        self.TWIDDLES = [W(i, self.FFT_SIZE) for i in range(self.FFT_HALF)]
+        self.TWIDDLES = [Complex(W(i, self.FFT_SIZE), 0, -8, overflow_style='saturate', round_style='round') for i in range(self.FFT_HALF)]
 
-        # self.coef = Complex()
-        # self.delay_shr = Complex()
-        # self.delay_x = Complex()
-        # self.delay_control = 0
+        self.twiddle_buffer = Complex()
 
     def butterfly(self, in_up, in_down, twiddle):
         up_real = resize(in_up.real + in_down.real, 0, -17)
@@ -43,28 +40,12 @@ class StageR2SDF(Hardware):
         down_real = resize((down_sub_real * twiddle.real) - (down_sub_imag * twiddle.imag), 0, -17)
         down_imag = resize((down_sub_real * twiddle.imag) + (down_sub_imag * twiddle.real), 0, -17)
         down = Complex(down_real, down_imag)
-
         return up, down
 
     def main(self, x, control):
-        # self.coef = self.TWIDDLES[control & self.CONTROL_MASK]
-        # self.delay_shr = self.shr[-1]
-        # self.delay_x = x
-        # self.delay_control = control
-        #
-        # up, down = self.butterfly(self.delay_shr, self.delay_x, self.coef)
-        # if self.FFT_HALF > 4:
-        #     down = down >> 1
-        #     up = up >> 1
-        #
-        # if not (self.delay_control & self.FFT_HALF):
-        #     self.shr = [self.delay_x] + self.shr[:-1]
-        #     return self.delay_shr
-        # else:
-        #     self.shr = [down] + self.shr[:-1]
-        #     return up
+        self.twiddle_buffer = self.TWIDDLES[(control + 1) & self.CONTROL_MASK]
+        up, down = self.butterfly(self.shr[-1], x, self.twiddle_buffer)
 
-        up, down = self.butterfly(self.shr[-1], x, self.TWIDDLES[control & self.CONTROL_MASK])
         if self.FFT_HALF > 4:
             down.real = down.real >> 1
             down.imag = down.imag >> 1
@@ -85,7 +66,9 @@ class R2SDF(Hardware):
 
         self.n_bits = int(np.log2(fft_size))
         # self.stages = [StageR2SDF(2 ** (pow + 1)) for pow in reversed(range(self.n_bits))]
-
+        #
+        # self.stage4000 = StageR2SDF(1024*4)
+        # self.stage2000 = StageR2SDF(1024*2)
         self.stage1024 = StageR2SDF(1024)
         self.stage512 = StageR2SDF(512)
         self.stage256 = StageR2SDF(256)
@@ -104,8 +87,10 @@ class R2SDF(Hardware):
         self.out = DataWithIndex(Complex(0.0, 0, -17), 0)
 
     def main(self, x):
-        # execute stages
+        # #execute stages
         out = x.data
+        # out = self.stage4000.main(out, x.index)
+        # out = self.stage2000.main(out, x.index)
         out = self.stage1024.main(out, x.index)
         out = self.stage512.main(out, x.index)
         out = self.stage256.main(out, x.index)
@@ -126,6 +111,7 @@ class R2SDF(Hardware):
         # new_index = (x.index + self.DELAY + 1) % self.FFT_SIZE
         self.out.data = out
         self.out.index = new_index
+        self.out.valid = x.valid
         return self.out
 
     def model_main(self, x):
@@ -149,18 +135,39 @@ class R2SDF(Hardware):
         return ffts
 
 
-@pytest.mark.parametrize("fft_size", [4, 8, 16, 32, 64, 128, 256])
+@pytest.mark.parametrize("fft_size", [2, 4, 8, 16, 32, 64, 128, 256])
 def test_fft(fft_size):
-    fft_size = 256
+    fft_size = 1024
     np.random.seed(0)
     dut = R2SDF(fft_size)
     inp = np.random.uniform(-1, 1, size=(2, fft_size)) + np.random.uniform(-1, 1, size=(2, fft_size)) * 1j
     inp *= 0.25
 
     sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
+                                           # 'RTL'
                                            'GATE'
                                            ],
                     conversion_path='/home/gaspar/git/pyhacores/playground',
+                    output_callback=DataWithIndex._pyha_unpack,
+                    input_callback=DataWithIndex._pyha_pack)
+    assert sims_close(sims, rtol=1e-1, atol=1e-4)
+
+
+
+def test_simple():
+    fft_size = 2
+    np.random.seed(0)
+    dut = R2SDF(fft_size)
+
+    inp = np.array([0+0.123j, 1+0.123j]).astype(complex) * 0.1
+    inp = [inp, inp]
+    # inp = np.random.uniform(-1, 1, size=(2, fft_size)) + np.random.uniform(-1, 1, size=(2, fft_size)) * 1j
+    # inp *= 0.1
+
+    sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
+                                           # 'GATE'
+                                           ],
+                    # conversion_path='/home/gaspar/git/pyhacores/playground',
                     output_callback=DataWithIndex._pyha_unpack,
                     input_callback=DataWithIndex._pyha_pack)
     assert sims_close(sims, rtol=1e-1, atol=1e-4)
--- under_construction/fft/windower.py
+++ under_construction/fft/windower.py
@@ -8,9 +8,10 @@ class Windower(Hardware):
     def __init__(self, M, window_type='hanning'):
         assert window_type == 'hanning'
         self.M = M
-        self.WINDOW = np.hanning(M)
+        self.window_pure = np.hanning(M)
+        self.WINDOW = [Sfix(x, 0, -7) for x in np.hanning(M)]
 
-        self.coef = Sfix(0.0, 0, -17)
+        self.coef = self.WINDOW[0]
         self.out = DataWithIndex(Complex(0.0, 0, -17))
         self.inp_delay = DataWithIndex(Complex(0.0, 0, -17))
         self.DELAY = 2
@@ -27,17 +28,20 @@ class Windower(Hardware):
         return self.out
 
     def model_main(self, complex_in_list):
-        return complex_in_list * self.WINDOW
+        return complex_in_list * self.window_pure
 
 
 @pytest.mark.parametrize("M", [4, 8, 16, 32, 64, 128, 256])
 def test_windower(M):
+    M = 1024 * 2 * 2 * 2
     dut = Windower(M)
     inp = np.random.uniform(-1, 1, size=(2, M)) + np.random.uniform(-1, 1, size=(2, M)) * 1j
 
     sims = simulate(dut, inp, simulations=['MODEL', 'PYHA',
-                                           'RTL'
+                                           # 'RTL',
+                                           'GATE'
                                            ],
+                    conversion_path='/home/gaspar/git/pyhacores/playground',
                     output_callback=DataWithIndex._pyha_unpack,
                     input_callback=DataWithIndex._pyha_pack)
 
