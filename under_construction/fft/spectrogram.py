b904767f424067ff2ee1908391f348aeeafd074f
Gaspar Karm <gkarm@live.com>
2018-04-05 22:33:36 +0300

    fix spectrogram

--- under_construction/fft/spectrogram.py
+++ under_construction/fft/spectrogram.py
@@ -1,4 +1,6 @@
 import numpy as np
+from data import load_iq
+
 from pyha import Hardware, simulate, hardware_sims_equal, sims_close
 from under_construction.fft.bitreversal_fftshift_decimate import BitreversalFFTshiftDecimate
 from under_construction.fft.conjmult import ConjMult
@@ -10,7 +12,7 @@ from scipy import signal
 
 class Spectrogram(Hardware):
     """ The gain of main/model_main wont match"""
-    def __init__(self, nfft, window_type='hanning', decimate_by=2):
+    def __init__(self, nfft, decimate_by=2, window_type='hanning'):
         self.DECIMATE_BY = decimate_by
         self.NFFT = nfft
         self.WINDOW_TYPE = window_type
@@ -48,16 +50,47 @@ class Spectrogram(Hardware):
 
 def test_simple():
     np.random.seed(0)
-    fft_size=128
-    dut = Spectrogram(fft_size)
+    fft_size=512
+    decimation=4
+    dut = Spectrogram(fft_size, decimation)
 
-    packets = 2
+    packets = 3
     inp = np.random.uniform(-1, 1, fft_size * packets) + np.random.uniform(-1, 1, fft_size * packets) * 1j
-    inp *= 0.25
+    inp *= 0.5
 
     sims = simulate(dut, inp,
                     output_callback=unpackage,
-                    simulations=['MODEL', 'PYHA', 'RTL'],
+                    simulations=['MODEL', 'PYHA'],
+                    conversion_path='/home/gaspar/git/pyhacores/playground')
+
+    # import matplotlib.pyplot as plt
+    # plt.plot(np.hstack(sims['MODEL']))
+    # plt.plot(np.hstack(sims['PYHA']))
+    # plt.plot(np.hstack(sims['RTL']))
+    # plt.show()
+
+    sims['MODEL'] = np.array(sims['MODEL']) / np.array(sims['MODEL']).max()
+    sims['PYHA'] = np.array(sims['PYHA']) / np.array(sims['PYHA']).max()
+    # sims['RTL'] = np.array(sims['RTL']) / np.array(sims['RTL']).max()
+    assert sims_close(sims, rtol=1e-1, atol=1e-4)
+
+
+def test_real_life():
+    fft_size=256
+    decimation=4
+    dut = Spectrogram(fft_size, decimation)
+
+    orig_inp = load_iq(
+        '/home/gaspar/Documents/Phantom 3 & 4=-90_20180104123014261769/1515061814.9318_qdetector_fs=20000000.0_bw=20000000.0_fc=2420000000.0_d=0.raw')
+
+    orig_inp = orig_inp[:16000//4]
+    orig_inp /= orig_inp.max() * 2
+
+    sims = simulate(dut, orig_inp,
+                    output_callback=unpackage,
+                    simulations=['MODEL', 'PYHA', 'RTL',
+                                 'GATE'
+                                 ],
                     conversion_path='/home/gaspar/git/pyhacores/playground')
 
     # import matplotlib.pyplot as plt
@@ -69,4 +102,5 @@ def test_simple():
     sims['MODEL'] = np.array(sims['MODEL']) / np.array(sims['MODEL']).max()
     sims['PYHA'] = np.array(sims['PYHA']) / np.array(sims['PYHA']).max()
     sims['RTL'] = np.array(sims['RTL']) / np.array(sims['RTL']).max()
+    sims['GATE'] = np.array(sims['GATE']) / np.array(sims['GATE']).max()
     assert sims_close(sims, rtol=1e-1, atol=1e-4)
