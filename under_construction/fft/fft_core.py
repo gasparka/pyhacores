77de35b04faa60d4f1c57747300b4ec89814fc90
Gaspar Karm <gkarm@live.com>
2018-05-17 18:03:54 +0300

    experiments

--- under_construction/fft/fft_core.py
+++ under_construction/fft/fft_core.py
@@ -17,27 +17,99 @@ def W(k, N):
     return np.exp(-1j * (2 * np.pi / N) * k)
 
 
+class StageR2SDFSFIX(Hardware):
+    def __init__(self, fft_size):
+        self.FFT_SIZE = fft_size
+        self.FFT_HALF = fft_size // 2
+
+        self.CONTROL_MASK = (self.FFT_HALF - 1)
+        self.shr = ShiftRegister([Complex() for _ in range(self.FFT_HALF)])
+
+        self.TWIDDLES = [Complex(W(i, self.FFT_SIZE), 0, -17, overflow_style='saturate', round_style='round') for i in range(self.FFT_HALF)]
+        # self.TWIDDLES = [W(i, self.FFT_SIZE) for i in range(self.FFT_HALF)]
+
+    def butterfly(self, in_up, in_down, twiddle):
+        up = resize(in_up + in_down, 0, -17) # make 0, -17 default?
+        down_part = resize(in_up - in_down, 0, -17)
+        down = resize(down_part * twiddle, 0, -17)
+        return up, down
+
+    def main(self, x, control):
+        if not (control & self.FFT_HALF):
+            self.shr.push_next(x)
+            return self.shr.peek()
+        else:
+            twid = self.TWIDDLES[control & self.CONTROL_MASK]
+            up, down = self.butterfly(self.shr.peek(), x, twid)
+
+            if self.FFT_HALF > 4:
+                down >>= 1
+                up >>= 1
+
+            self.shr.push_next(down)
+            return up
+
+
+class R2SDFSFIX(Hardware):
+    def __init__(self, fft_size):
+        self.FFT_SIZE = fft_size
+
+        self.n_bits = int(np.log2(fft_size))
+        self.stages = [StageR2SDFSFIX(2 ** (pow + 1)) for pow in reversed(range(self.n_bits))]
+
+        # Note: it is NOT correct to use this gain after the magnitude/abs operation, it has to be applied to complex values
+        self.GAIN_CORRECTION = 2 ** (0 if self.n_bits - 3 < 0 else -(self.n_bits - 3))
+        self.DELAY = (fft_size - 1) + 1  # +1 is output register
+
+        self.out = DataWithIndex(Complex(0.0, 0, -17), 0)
+
+    def main(self, x):
+        # #execute stages
+        out = x.data
+        for stage in self.stages:
+            out = stage.main(out, x.index)
+
+        self.out.data = out
+        self.out.index = (x.index + self.DELAY + 1) % self.FFT_SIZE
+        self.out.valid = x.valid
+        return self.out
+
+    def model_main(self, x):
+        ffts = np.fft.fft(x)
+
+        # apply bit reversing ie. mess up the output order to match radix-2 algorithm
+        # from under_construction.fft.bit_reversal import bit_reversed_indexes
+        def bit_reverse(x, n_bits):
+            return int(np.binary_repr(x, n_bits)[::-1], 2)
+
+        def bit_reversed_indexes(N):
+            return [bit_reverse(i, int(np.log2(N))) for i in range(N)]
+
+        rev_index = bit_reversed_indexes(self.FFT_SIZE)
+        for i, _ in enumerate(ffts):
+            ffts[i] = ffts[i][rev_index]
+
+        # apply gain control (to avoid overflows in hardware)
+        ffts *= self.GAIN_CORRECTION
+
+        return ffts
+
+
 class StageR2SDF(Hardware):
     def __init__(self, fft_size):
         self.FFT_SIZE = fft_size
         self.FFT_HALF = fft_size // 2
 
         self.CONTROL_MASK = (self.FFT_HALF - 1)
-        # self.shr = ShiftRegister([Complex() for _ in range(self.FFT_HALF)])
         self.shr = ShiftRegister([ComplexFloat() for _ in range(self.FFT_HALF)])
 
-        # print(mp)
-        # self.TWIDDLES = [Complex(W(i, self.FFT_SIZE), 0, -8, overflow_style='saturate', round_style='round') for i in range(self.FFT_HALF)]
-        self.TWIDDLES = [ComplexFloat(W(i, self.FFT_SIZE)) for i in range(self.FFT_HALF)]
+        self.TWIDDLES = [Complex(W(i, self.FFT_SIZE), 0, -5, overflow_style='saturate', round_style='round') for i in range(self.FFT_HALF)]
+        # self.TWIDDLES = [ComplexFloat(W(i, self.FFT_SIZE)) for i in range(self.FFT_HALF)]
         # self.TWIDDLES = [W(i, self.FFT_SIZE) for i in range(self.FFT_HALF)]
 
     def butterfly(self, in_up, in_down, twiddle):
-        # up = resize(in_up + in_down, 0, -17) # make 0, -17 default?
         up = in_up + in_down
-        #
-        # down_part = resize(in_up - in_down, 0, -17)
         down_part = in_up - in_down
-        # down = resize(down_part * twiddle, 0, -17)
         down = down_part * twiddle
         return up, down
 
@@ -68,7 +140,6 @@ class R2SDF(Hardware):
         self.GAIN_CORRECTION = 2 ** (0 if self.n_bits - 3 < 0 else -(self.n_bits - 3))
         self.DELAY = (fft_size - 1) + 1  # +1 is output register
 
-        # self.out = DataWithIndex(Complex(0.0, 0, -17), 0)
         self.out = DataWithIndex(ComplexFloat(), 0)
 
     def main(self, x):
@@ -103,6 +174,20 @@ class R2SDF(Hardware):
         return ffts
 
 
+def test_floats():
+    fft_size = 1024
+
+    sig = np.exp(2j * np.pi * np.linspace(0, 1, fft_size) * 200)
+    for i in range(100, 300, 8):
+        sig2 = np.exp(2j * np.pi * np.linspace(0, 1, fft_size) * i * -1)
+        sig = sig + sig2
+
+    sig *= 1 / 2 / 2 / 2 / 2 / 2 / 2
+    sig *= np.hanning(fft_size)
+    dut = R2SDF(fft_size)
+    sims = simulate(dut, sig, input_types=[ComplexFloat()], simulations=['PYHA'], output_callback=unpackage,
+                    input_callback=package)
+    pass
 # 9 bit (MAX)
 # MAX clk: 22MHz
 # Max Sine peak: ~56 dB
