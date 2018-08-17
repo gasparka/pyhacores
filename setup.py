5db0d71386807d114ef31cf5cfd2372606d00040
Gaspar Karm <gkarm@live.com>
2018-08-17 16:53:33 +0300

    start spectrogram refactor

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
